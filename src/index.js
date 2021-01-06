86696cb255db9ac29cce4550319c654565df37dd
yoshiki-tanaka999 <shootingstarorufo@gmail.com>
2021-01-06 18:26:45 +0900

    微調整

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-
 function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
@@ -124,10 +123,6 @@ class Game extends React.Component {
   }
 }
 
-// ========================================
-
-ReactDOM.render(<Game />, document.getElementById("root"));
-
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
@@ -147,3 +142,7 @@ function calculateWinner(squares) {
   }
   return null;
 }
+
+// ========================================
+
+ReactDOM.render(<Game />, document.getElementById("root"));
